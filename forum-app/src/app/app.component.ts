481522f8a3557971e10a49c54c4d6c57bfd54fff
Avstrian <dimitarzhelyazkov3003@gmail.com>
2022-03-16 22:29:15 +0200

    Basic setup

---
+++ forum-app/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: [
+    './app.component.css', './layout.css', './reset.css', './responsive.css',
+    './style.css', './typography.css'
+  ]
+})
+export class AppComponent {
+  title = 'forum-app';
+}
